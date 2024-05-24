baa61f5c7397af90ce3fcf9a798215a5ef53cef6
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-05-23 20:42:08 -0500

    wipsorry

--- lib/src/flakka_application.dart
+++ lib/src/flakka_application.dart
@@ -1,7 +1,19 @@
+import 'package:fpdart/fpdart.dart';
+
 /// {@template flakka_application}
 /// Create Dart Project Repo and push template generator output
 /// {@endtemplate}
 class FlakkaApplication {
   /// {@macro flakka_application}
   const FlakkaApplication();
+
+  ///
+  Future<Exit<Error, void>> mutate<Command, Error>(Command command) async {
+    throw UnimplementedError();
+  }
+
+  ///
+  View query<Query, View>(Query query) {
+    throw UnimplementedError();
+  }
 }
